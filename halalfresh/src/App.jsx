e5edb77380d605074ecc988e29a37a6ed044d3a2
Anzal-F <anzalmohamed09@gmail.com>
2024-02-17 17:58:23 -0500

    commit

--- halalfresh/src/App.jsx
+++ halalfresh/src/App.jsx
@@ -11,6 +11,7 @@ import Footer from './components/footer/footer';
 
       const App = () => {
   return (
+    <>
     <div className="App">
       <Router>
       <Navigation />
@@ -23,86 +24,7 @@ import Footer from './components/footer/footer';
         {/* Add routes for other pages */}
       </Routes>
       <Footer/>
-      </Router>
-    </div>
-  );
-};
-
-      <div className="card-container">
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={reactLogo} />
-          <Card.Body>
-            <Card.Title>Breakfast</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content. Nice!
-            </Card.Text>
-            <Dropdown>
-              <Dropdown.Toggle variant="primary" id="dropdown-basic">
-                Select Option
-              </Dropdown.Toggle>
-
-              <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Option 1</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item href="#/action-2">Option 2</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item href="#/action-3">Option 3</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Card.Body>
-        </Card>
-
-        {/* Lunch Card */}
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={reactLogo} />
-          <Card.Body>
-            <Card.Title>Lunch</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Dropdown>
-              <Dropdown.Toggle variant="primary" id="dropdown-basic">
-                Select Option
-              </Dropdown.Toggle>
-
-              <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Option 1</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item href="#/action-2">Option 2</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item href="#/action-3">Option 3</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Card.Body>
-        </Card>
-
-        {/* Dinner Card */}
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={reactLogo} />
-          <Card.Body>
-            <Card.Title>Dinner</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Dropdown>
-              <Dropdown.Toggle variant="primary" id="dropdown-basic">
-                Select Option
-              </Dropdown.Toggle>
-
-              <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Option 1</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item href="#/action-2">Option 2</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item href="#/action-3">Option 3</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Card.Body>
-        </Card>
-      </div>
+      </Router>   </div>
     </>
   );
 };
