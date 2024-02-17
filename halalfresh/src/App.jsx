fb5cdd4ad6ecb94f25888173096196575a1fea1c
Anzal-F <anzalmohamed09@gmail.com>
2024-02-17 14:15:54 -0500

    test

--- halalfresh/src/App.jsx
+++ halalfresh/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
-import { Button, Card } from 'react-bootstrap'; // Import Button and Card from react-bootstrap
+import { Button, Card, Dropdown } from 'react-bootstrap'; // Import Button, Card, and Dropdown from react-bootstrap
 
 function App() {
   const [count, setCount] = useState(0);
@@ -17,10 +17,23 @@ function App() {
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+            <Dropdown>
+              <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                Select Option
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Option 1</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item href="#/action-2">Option 2</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item href="#/action-3">Option 3</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
           </Card.Body>
         </Card>
 
+        {/* Lunch Card */}
         <Card style={{ width: '18rem' }}>
           <Card.Img variant="top" src={reactLogo} />
           <Card.Body>
@@ -29,10 +42,23 @@ function App() {
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+            <Dropdown>
+              <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                Select Option
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Option 1</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item href="#/action-2">Option 2</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item href="#/action-3">Option 3</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
           </Card.Body>
         </Card>
 
+        {/* Dinner Card */}
         <Card style={{ width: '18rem' }}>
           <Card.Img variant="top" src={reactLogo} />
           <Card.Body>
@@ -41,7 +67,19 @@ function App() {
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
+            <Dropdown>
+              <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                Select Option
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">Option 1</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item href="#/action-2">Option 2</Dropdown.Item>
+                <Dropdown.Divider />
+                <Dropdown.Item href="#/action-3">Option 3</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
           </Card.Body>
         </Card>
       </div>
