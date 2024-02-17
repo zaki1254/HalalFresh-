01f8ac129f5951c3feb5f7de31768e6531140ea4
zaki1254 <zakessa10@gmail.com>
2024-02-17 17:54:42 -0500

    merge

--- halalfresh/src/App.jsx
+++ halalfresh/src/App.jsx
@@ -8,102 +8,26 @@ import Contact from './components/pages/contact';
 import Register from './components/pages/register';
 import StartOrder from './components/pages/startOrder';
 import Footer from './components/footer/footer';
+import { Button, Card, Dropdown } from 'react-bootstrap'; // Import Button, Card, and Dropdown from react-bootstrap
 
-      const App = () => {
+function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
       <Router>
-      <Navigation />
-      <Routes>
-        <Route path="/ourMenu" element={<Menu />} />
-        <Route path="/FAQs" element={<FAQs />} /> {/* Add route for FAQs page */}
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/startOrder" element={<StartOrder />} />
-        {/* Add routes for other pages */}
-      </Routes>
-      <Footer/>
+        <Navigation />
+        <Routes>
+          <Route path="/our-menu" element={<Menu />} />
+          <Route path="/FAQs" element={<FAQs />} /> {/* Add route for FAQs page */}
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/startOrder" element={<StartOrder />} />
+          {/* Add routes for other pages */}
+        </Routes>
+        <Footer/>
       </Router>
     </div>
-  );
-};
-
-      <div className="card-container">
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={reactLogo} />
-          <Card.Body>
-            <Card.Title>Breakfast</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content. Nice!
-            </Card.Text>
-            <Dropdown>
-              <Dropdown.Toggle variant="primary" id="dropdown-basic">
-                Select Option
-              </Dropdown.Toggle>
-
-              <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Option 1</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item href="#/action-2">Option 2</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item href="#/action-3">Option 3</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Card.Body>
-        </Card>
-
-        {/* Lunch Card */}
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={reactLogo} />
-          <Card.Body>
-            <Card.Title>Lunch</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Dropdown>
-              <Dropdown.Toggle variant="primary" id="dropdown-basic">
-                Select Option
-              </Dropdown.Toggle>
-
-              <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Option 1</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item href="#/action-2">Option 2</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item href="#/action-3">Option 3</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Card.Body>
-        </Card>
-
-        {/* Dinner Card */}
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={reactLogo} />
-          <Card.Body>
-            <Card.Title>Dinner</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Dropdown>
-              <Dropdown.Toggle variant="primary" id="dropdown-basic">
-                Select Option
-              </Dropdown.Toggle>
-
-              <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Option 1</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item href="#/action-2">Option 2</Dropdown.Item>
-                <Dropdown.Divider />
-                <Dropdown.Item href="#/action-3">Option 3</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </Card.Body>
-        </Card>
-      </div>
-    </>
   );
 };
 
