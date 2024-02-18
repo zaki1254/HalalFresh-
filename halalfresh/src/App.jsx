e0c9b2709f646648295426e271d9b587819c0b01
Anzal-F <anzalmohamed09@gmail.com>
2024-02-18 10:59:05 -0500

    commit

--- halalfresh/src/App.jsx
+++ halalfresh/src/App.jsx
@@ -1,37 +1,30 @@
 import React from 'react';
-import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './components/navigation/navigation';
+import CardComponent from './components/card/card'; // Import the Card component
+import Home from './components/home/Home'; // Import the Home component
 import FAQs from './components/pages/faq'; // Import the FAQs component
-import Menu from './components/pages/menu'; // Assuming you have other pages
 import Contact from './components/pages/contact';
 import Register from './components/pages/register';
 import StartOrder from './components/pages/startOrder';
 import Footer from './components/footer/footer';
-import { Button, Card, Dropdown } from 'react-bootstrap'; // Import Button, Card, and Dropdown from react-bootstrap
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
     <div className="App">
       <Router>
-      <Navigation />
-      <Routes>
-        <Route path="/ourMenu" element={<Menu />} />
-        <Route path="/FAQs" element={<FAQs />} /> {/* Add route for FAQs page */}
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/startOrder" element={<StartOrder />} />
-        {/* Add routes for other pages */}
-      </Routes>
-      <Footer/>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Set CardComponent as the landing page */}
+          <Route path="/FAQs" element={<FAQs />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/startOrder" element={<StartOrder />} />
+        </Routes>
+        <Footer />
       </Router>
     </div>
-    </>
   );
-};
-
+}
 
 export default App;
